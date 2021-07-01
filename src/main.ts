1f2a271fab35f714d5fe57e310fa77ccdfc9bdfe
mide7 <imharyomide@gmail.com>
2021-07-01 07:29:49 +0100

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Todo App')
+  .setDescription('My Todo API description')
+  .setVersion('0.0.1')
+  .addTag('tasks')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
+
   app.enableCors()
   await app.listen(process.env.PORT || 5000);
 }
